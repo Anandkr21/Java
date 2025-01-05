45a6f6f4c90db265d93af571a5a59ef514d8fbbd
Anand Kumar <anandkumargupta.akg15@gmail.com>
2025-01-05 19:48:42 +0530

    loops array and function

---
+++ LoopsArrayFunction/src/Operators.java
@@ -0,0 +1,23 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("hello");
+		
+		// >, <, >=, <=, ==, !=
+		// &, && , ||, |
+		
+		System.out.println(6>=6);
+		
+		int a = 10;
+		int b = 6;
+		
+		if(a != b) {
+			System.out.println("Both are not equal");
+		}
+		System.out.println(a%b);
+
+	}
+
+}
